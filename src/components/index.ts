6e5ff471c3ee75ff76c454443db7fa7d44a6f4d5
Rajommkar <marthaommkarnath@gmail.com>
2026-06-12 13:22:53 +0530

    feat: implement chapter 5 dashboard components and layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,24 @@
 export { Header } from "./header";
+import Profile from "./common/Profile";
+import PropertyCard from "./common/PropertyCard";
+import CustomButton from "./common/CustomButton";
+import Form from "./common/Form";
+
+import PieChart from "./charts/PieChart";
+import PropertyReferrals from "./charts/PropertyReferrals";
+import TotalRevenue from "./charts/TotalRevenue";
+
+import AgentCard from "./agent/AgentCard";
+import TopAgent from "./home/TopAgent";
+
+export {
+  Profile,
+  PropertyCard,
+  CustomButton,
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  AgentCard,
+  TopAgent,
+  Form,
+};
